15407560b8aeeb6aa14cf0063454e2f968db07c9
Saul <221k0038@itscarrillopuerto,edu.mx>
2026-02-25 18:42:41 -0500

    a

---
+++ Tema 1/TemaUno_Lenguajes/Ejercicios_1.2.1/aritméticas.js
@@ -0,0 +1,30 @@
+/*
+Ejercicio i
+Funciones aritméticas básicas en un solo archivo
+*/
+
+// Función suma
+function suma(a, b) {
+  return a + b;
+}
+
+// Función resta
+function resta(a, b) {
+  return a - b;
+}
+
+// Función multiplicación
+function multiplicacion(a, b) {
+  return a * b;
+}
+
+// Función división
+function division(a, b) {
+  return a / b;
+}
+
+// Mostrar resultados en consola
+console.log("Suma:", suma(10, 5));
+console.log("Resta:", resta(10, 5));
+console.log("Multiplicación:", multiplicacion(10, 5));
+console.log("División:", division(10, 5));
