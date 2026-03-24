567ceeca811bf9e36bda5ddc0bcd3b1d17dbf00d
221k0038-Alexander <221k0038@itscarrillopuerto.edu.mx>
2026-03-23 23:49:56 -0500

    Add files via upload

---
+++ TemaDos_APIS/Ejercicio 2.1/conversio2.js
@@ -0,0 +1,9 @@
+// Saul Alexander Che Caamal
+
+// Cadena JSON
+const jsonString = '{"nombre":"Taco de Pollo","ingredientes": {"proteina":"Pollo", "salsa":"Salsa Verde"}}';
+
+// Deserializar para convertir a objeto JavaScript
+const objetoDeserializado = JSON.Parse(jsonString);
+
+consola.  log(objetoDeserializado);
---
+++ TemaDos_APIS/Ejercicio 2.1/conversion1.js
@@ -0,0 +1,12 @@
+// Saul Alexander Che Caamal
+const  objetoJavaScript = {
+    nombre: "Taco de Pollo",
+    Ingredientes: {
+        proteina: "Pollo",
+        salsa: "Salsa Verde"
+    }
+};
+
+const jsonString = JSON.  stringify(objetoJavaScript);
+
+consola.  log(jsonString);
